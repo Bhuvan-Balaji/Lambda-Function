4bd96ac37969cd75e9bb9d8dc963d5abdf6538e3
Bhuvan-Balaji <45603881+Bhuvan-Balaji@users.noreply.github.com>
2021-06-28 13:21:13 +0530

    Update App.java

--- Lambda Function/src/main/java/com/test/App.java
+++ Lambda Function/src/main/java/com/test/App.java
@@ -47,93 +47,83 @@ public class App implements RequestHandler<S3EventNotification, String> {
 
 
 	public String handleRequest(S3EventNotification s3Event, Context context) {
-		log.info("Lambda function is invoked:" + s3Event.toJson());
 		
+		log.info("Lambda function is invoked:" + s3Event.toJson());
 		
 		
 		//Get the File details which triggered the Lambda Function
 		S3EventNotification.S3EventNotificationRecord record = s3Event.getRecords().get(0);
-        String bucketName = record.getS3().getBucket().getName();
-        String keyName = record.getS3().getObject().getKey().replace('+', ' ');
-        try {
+       		String bucketName = record.getS3().getBucket().getName();
+        	String keyName = record.getS3().getObject().getKey().replace('+', ' ');
+		
+		//Encrypting below
+        	try {
 			keyName = URLDecoder.decode(keyName, "UTF-8");
 			
-			 AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
+			AWSCredentials credentials = new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY);
 		        
 		        KeyGenerator symKeyGenerator;
 		        SecretKey symKey_file;
 			
-					symKeyGenerator = KeyGenerator.getInstance("AES");
-					symKeyGenerator.init(256);
-					symKey_file = symKeyGenerator.generateKey();
+			symKeyGenerator = KeyGenerator.getInstance("AES");
+			symKeyGenerator.init(256);
+			symKey_file = symKeyGenerator.generateKey();
 					
-					String encodedKey = Base64.getEncoder().encodeToString(symKey_file.getEncoded());
+			String encodedKey = Base64.getEncoder().encodeToString(symKey_file.getEncoded());
 					
-					 EncryptionMaterials encryptionMaterials = new EncryptionMaterials(symKey_file);
-					 AmazonS3 s3EncryptionClient = AmazonS3EncryptionClientBuilder.standard()
+			EncryptionMaterials encryptionMaterials = new EncryptionMaterials(symKey_file);
+			AmazonS3 s3EncryptionClient = AmazonS3EncryptionClientBuilder.standard()
 					            .withCredentials(new AWSStaticCredentialsProvider(credentials))
 					            .withEncryptionMaterials(new StaticEncryptionMaterialsProvider(encryptionMaterials))
 					            .withRegion(CLIENT_REGION)
 					            .build();
 					 
-					 AmazonS3 s3Client = AmazonS3ClientBuilder.standard().
-							 withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(CLIENT_REGION).build();
+			AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+				 	     .withCredentials(new AWSStaticCredentialsProvider(credentials))
+				 	     .withRegion(CLIENT_REGION).build();
 					 
-					 S3Object downloadedObject = s3Client.getObject(bucketName, keyName); 
-					 ObjectMetadata metadata = s3Client.getObjectMetadata(bucketName, keyName);
-					 S3ObjectInputStream inputStream = downloadedObject.getObjectContent();
+			S3Object downloadedObject = s3Client.getObject(bucketName, keyName); 
+			ObjectMetadata metadata = s3Client.getObjectMetadata(bucketName, keyName);
+			S3ObjectInputStream inputStream = downloadedObject.getObjectContent();
 					 
 					 
 					 
-					 s3EncryptionClient.putObject(new PutObjectRequest(
+			s3EncryptionClient.putObject(new PutObjectRequest(
 					            BUCKET_NAME,
 					            keyName,
 					           inputStream,
 					            metadata));
 					 
-	     log.info("File encrypted and uploaded into the S3 bucket");
-	     
-					 
-					 
-					 
-					 AWSKMS kmsClient = AWSKMSClientBuilder.standard()
+	    		log.info("File encrypted and uploaded into the S3 bucket");
+	     			 					 
+			AWSKMS kmsClient = AWSKMSClientBuilder.standard()
 								.withRegion(CLIENT_REGION)
 								.withCredentials(new AWSStaticCredentialsProvider(credentials))
 								.build();
-
-						String s3ObjectKeyName = keyName + "Key";
-						String s3ObjectContent = encodedKey;
-
-						AmazonS3 s3EncryptionClient_key = AmazonS3EncryptionClientBuilder.standard()
-								.withCredentials(new AWSStaticCredentialsProvider(credentials))
-								.withEncryptionMaterials(new KMSEncryptionMaterialsProvider(CMK_KEY_ARN))
-								.withRegion(CLIENT_REGION)
-								.withKmsClient(kmsClient)
-								.build();
-
-
-						s3EncryptionClient_key.putObject(BUCKET_NAME, s3ObjectKeyName, s3ObjectContent);
-
-		log.info("Key encrypted and uploaded into S3 bucket");
-						
-        
-        
-        
-       
-			} catch (NoSuchAlgorithmException | IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
 			
+			String s3ObjectKeyName = keyName + "Key";
+			String s3ObjectContent = encodedKey;
+			
+			AmazonS3 s3EncryptionClient_key = AmazonS3EncryptionClientBuilder.standard()
+			    				  .withCredentials(new AWSStaticCredentialsProvider(credentials))
+							  .withEncryptionMaterials(new KMSEncryptionMaterialsProvider(CMK_KEY_ARN))
+							  .withRegion(CLIENT_REGION)
+							  .withKmsClient(kmsClient)
+							  .build();
+
+
+			s3EncryptionClient_key.putObject(BUCKET_NAME, s3ObjectKeyName, s3ObjectContent);
+
+			log.info("Key encrypted and uploaded into S3 bucket");
 		
-		
-		
-		
-        
-        
-		
-		
-		
+			s3Client.deleteObject(bucketName, keyName);
+	     			
+	     		log.info("Unencrypted File deleted");
+			
+       		} catch (NoSuchAlgorithmException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		return null;
 	}
